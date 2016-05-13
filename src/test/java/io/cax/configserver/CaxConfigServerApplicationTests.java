edefd4c6cf07eba306f1409b67f3872f3849b526
Carlos Queiroz <caxqueiroz@gmail.com>
2016-05-13 11:04:19 +0800

    First commit

---
+++ src/test/java/io/cax/configserver/CaxConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.cax.configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ConfigServerApp.class)
+public class CaxConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
